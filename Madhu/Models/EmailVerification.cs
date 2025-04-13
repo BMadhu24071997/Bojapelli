fc7d41cdbbc8a55e88a8574e2b09e0757f0df75f
madhu.up.mb@gmail.com <madhu.up.mb@gmail.com>
2025-04-13 13:55:54 +0530

    emailverification model created

---
+++ Madhu/Models/EmailVerification.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Madhu.Models
+{
+    public class EmailVerification
+    {
+        [Key]
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public int OTP { get; set; }
+        public DateTime OTPGeneratedDateTime { get; set; } = DateTime.Now;
+
+
+    }
+}
