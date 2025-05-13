9578c7eab950e07f09365b513d6d50f81ddece6d
madhu.up.mb@gmail.com <madhu.up.mb@gmail.com>
2025-05-13 23:21:16 +0530

    Adding Products in Product Controllers

---
+++ Madhu/Controllers/ProductController.cs
@@ -0,0 +1,31 @@
+﻿using Madhu.Interfaces;
+using Madhu.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Madhu.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly IProductService _productService;
+        private readonly ApplicationDbContext _db;
+
+        public ProductController(IProductService productService, ApplicationDbContext db)
+        {
+            _productService = productService;
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            var user = _db.Users.Find(HttpContext.Session.GetString("UserName"));
+            //var cartItems = _db.Cart.Where(c => c.Username == user.UserName && c.Status == "Added To Cart").ToList();
+           // ViewBag.TotalProductss = cartItems.Sum(c => c.Quantity);
+
+
+            var products = _productService.GetProducts(); // Fetch product list
+            return View(products);
+        }
+
+    }
+}
+
